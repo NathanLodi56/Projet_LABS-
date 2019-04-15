5ec1a6e95b9fc03e065e71463ae8513fb7f73362
NathanNdjoni <brakiplace@gmail.com>
2019-04-15 17:48:40 +0200

    1. creation lab-theme +prends js,css+fonts+img+ faire des templates + header + get_ tempalte_part

---
+++ wp-content/themes/labs-theme/index.php
@@ -0,0 +1,13 @@
+<?php
+
+get_header();
+
+get_template_part("templates/intro_section.php");
+get_template_part("templates/section_about.php");
+get_template_part("templates/testimonial_section.php");
+get_template_part("templates/services_section.php");
+get_template_part("templates/team_section.php");
+get_template_part("templates/promotion_section.php").
+get_template_part("templates/contact_section.php");
+
+get_footer();
